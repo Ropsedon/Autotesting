d4bfb16ca49b4dbe9d7d2f5ab8b42405874be66c
a.glukhov <glukhov.bank@gmail.com>
2018-10-12 16:23:50 +0300

    Changed test

--- add_group.py
+++ add_group.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from group import Group
 
@@ -14,20 +12,14 @@ class UntitledTestCase(unittest.TestCase):
     
     def test_add_group(self):
         driver = self.driver
-        self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
         self.create_group(driver, Group(name="sdfafas", header="asdfsa", footer="asdfsadf"))
-        self.return_to_groups_page(driver)
         self.logout(driver)
 
     def test_add_empty_group(self):
         driver = self.driver
-        self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
-        self.open_groups_page(driver)
         self.create_group(driver, Group(name="", header="", footer=""))
-        self.return_to_groups_page(driver)
         self.logout(driver)
 
     def logout(self, driver):
@@ -38,6 +30,7 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_link_text("groups").click()
 
     def create_group(self, driver, group):
+        self.open_groups_page(driver)
         # Init group creation
         driver.find_element_by_name("new").click()
         # Fill group form
@@ -52,11 +45,13 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("group_footer").send_keys(group.footer)
         # Submit group creation
         driver.find_element_by_name("submit").click()
+        self.return_to_groups_page(driver)
 
     def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
 
     def login(self, driver, username, password):
+        self.open_home_page(driver)
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys(username)
@@ -67,27 +62,6 @@ class UntitledTestCase(unittest.TestCase):
 
     def open_home_page(self, driver):
         driver.get("http://127.0.0.1/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
     
     def tearDown(self):
         self.driver.quit()

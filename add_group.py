94a9988039a0a13c6d3151da6d297615aa262dba
a.glukhov <glukhov.bank@gmail.com>
2018-10-12 15:24:38 +0300

    Improve first test

--- add_group.py
+++ add_group.py
@@ -1,44 +1,72 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
-        self.accept_next_alert = True
     
-    def test_untitled_test_case(self):
+    def test_add_group(self):
         driver = self.driver
-        driver.get("http://127.0.0.1/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_id("LoginForm").submit()
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, name="sdfafas", header="asdfsa", footer="asdfsadf")
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, name="", header="", footer="")
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, driver):
+        # Return to groups page
         driver.find_element_by_link_text("groups").click()
+
+    def create_group(self, driver, name, header, footer):
+        # Init group creation
         driver.find_element_by_name("new").click()
+        # Fill group form
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("sdfafas")
+        driver.find_element_by_name("group_name").send_keys(name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("asdfsa")
+        driver.find_element_by_name("group_header").send_keys(header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("asdfsadf")
+        driver.find_element_by_name("group_footer").send_keys(footer)
+        # Submit group creation
         driver.find_element_by_name("submit").click()
+
+    def open_groups_page(self, driver):
         driver.find_element_by_link_text("groups").click()
-    
+
+    def login(self, driver, username, password):
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_id("LoginForm").submit()
+
+    def open_home_page(self, driver):
+        driver.get("http://127.0.0.1/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
@@ -63,6 +91,7 @@ class UntitledTestCase(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()

e18cd730e9a8274dcfc26be7035f0997879e9028
a.glukhov <glukhov.bank@gmail.com>
2018-10-16 13:52:49 +0300

    Dedicated class for work with group

---
+++ fixture/group.py
@@ -0,0 +1,32 @@
+class GroupHelper(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_groups_page(self):
+        # Return to groups page
+        driver = self.app.driver
+        driver.find_element_by_link_text("groups").click()
+
+    def create(self, group):
+        driver = self.app.driver
+        self.open_groups_page()
+        # Init group creation
+        driver.find_element_by_name("new").click()
+        # Fill group form
+        driver.find_element_by_name("group_name").click()
+        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_name("group_name").send_keys(group.name)
+        driver.find_element_by_name("group_header").click()
+        driver.find_element_by_name("group_header").clear()
+        driver.find_element_by_name("group_header").send_keys(group.header)
+        driver.find_element_by_name("group_footer").click()
+        driver.find_element_by_name("group_footer").clear()
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        # Submit group creation
+        driver.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        driver = self.app.driver
+        driver.find_element_by_link_text("groups").click()
--- test/add_group.py
+++ test/add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="sdfafas", header="asdfsa", footer="asdfsadf"))
+    app.group.create(Group(name="sdfafas", header="asdfsa", footer="asdfsadf"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 

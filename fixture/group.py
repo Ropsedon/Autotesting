d5f645fe8c22a7d62d89ba936fc29ea997432235
a.glukhov <glukhov.bank@gmail.com>
2018-10-16 17:18:57 +0300

    Add new test delete first group

--- fixture/group.py
+++ fixture/group.py
@@ -30,3 +30,12 @@ class GroupHelper(object):
     def open_groups_page(self):
         driver = self.app.driver
         driver.find_element_by_link_text("groups").click()
+
+    def delete_first_group(self):
+        driver = self.app.driver
+        self.open_groups_page()
+        # select first group
+        driver.find_element_by_name("selected").click()
+        # submit deletion
+        driver.find_element_by_name("delete").click()
+        self.return_to_groups_page()
